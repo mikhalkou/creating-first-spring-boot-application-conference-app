4cfdd079ccaf81f1b2364f7c28caaf6fa1ee1758
Maksim Mikhalkou <maksim@mikhalkou.com>
2021-04-30 01:37:41 +0200

    Init project

---
+++ src/main/java/com/mikhalkou/learn/springboot1/conferencespringboot/ConferenceSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.mikhalkou.learn.springboot1.conferencespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenceSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mikhalkou/learn/springboot1/conferencespringboot/ConferenceSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikhalkou.learn.springboot1.conferencespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
